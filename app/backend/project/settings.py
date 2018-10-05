8780e1b1cf688e12e4aeccc7f804f35d71d95a26
Mitchell Mosure <hctimit@gmail.com>
2018-10-05 12:07:33 -0500

    setup: Renamed folders/modules

--- app/backend/project/settings.py
+++ app/backend/project/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'nano_speedtest.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'nano_speedtest.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
