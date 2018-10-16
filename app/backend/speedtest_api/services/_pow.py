1537d402863c5311925248e08ccf0c97754a07b5
Mitchell Mosure <hctimit@gmail.com>
2018-10-16 18:37:01 -0500

    Added docstring

--- app/backend/speedtest_api/services/_pow.py
+++ app/backend/speedtest_api/services/_pow.py
@@ -2,7 +2,13 @@ import requests
 from django.conf import settings as settings
 
 
-# Returns a json object with a 'work' property containing the POW
+"""
+Generate a PoW using the distributed endpoint.
+
+@param hash: Hash to generate PoW for
+@return: Json object containing a work property
+@raise httpError: Can't connect to dPoW, or dPoW timed out
+"""
 def get_pow(hash):
     data = {
             'hash': hash,
