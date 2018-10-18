4080be3597e466e55b2eaf0100fc99e67abbb6a2
Matty Caulfield <caulfield2@wisc.edu>
2018-10-18 14:40:43 -0500

    Removed serializers and views folders for now, added the api app to the settings file, added .idea/ to the .gitignore

--- app/backend/project/settings.py
+++ app/backend/project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'speedtest_api',
 ]
 
 MIDDLEWARE = [
--- app/backend/speedtest_api/views.py
+++ app/backend/speedtest_api/views.py
@@ -23,6 +23,6 @@ def send_transaction():
 
     serializer = TransactionSerializer(data=transaction)
     if serializer.is_valid():
-        #serializer.save
+        # serializer.save
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
