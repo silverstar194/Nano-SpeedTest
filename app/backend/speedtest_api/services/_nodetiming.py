9ae2e3f69eb28daca2cc7a92c1d7ea06ae7db965
Admin <admin@Admins-MacBook-Pro.local>
2018-10-25 14:48:54 -0500

    dummy method for time_transaction

---
+++ app/backend/speedtest_api/services/_nodetiming.py
@@ -0,0 +1,11 @@
+#TODO include the python wrapper here
+import random
+
+
+def time_transaction(transaction_hash):
+	"""This will return the time delta in the of the sending and recieving block
+	@param transaction_hash
+	@return time_delta
+	"""
+	#TODO implement timing
+	return random.randint(1,5) + random.randint(0,10) / 10.0	
