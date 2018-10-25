0bc71aed8d3a011bc67fedd086d25d5eb06773d2
Mitchell Mosure <hctimit@gmail.com>
2018-10-24 21:27:32 -0500

    fix: changed POW field to be 16 characters

--- app/backend/speedtest_api/models/account.py
+++ app/backend/speedtest_api/models/account.py
@@ -6,4 +6,4 @@ from .node import Node
 class Account(models.Model):
     node = models.ForeignKey(Node, on_delete=models.PROTECT)
     current_balance = models.IntegerField()  # Measured in RAW
-    POW = models.CharField(max_length=256)
+    POW = models.CharField(max_length=16)
