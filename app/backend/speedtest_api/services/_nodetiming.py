2d37015b76ce37f94583cd7a0960b60fa7112157
Soph-PhiaC <sophiacarayannopoulos@gmail.com>
2018-10-30 18:24:26 -0500

    Added docstr

--- app/backend/speedtest_api/services/_nodetiming.py
+++ app/backend/speedtest_api/services/_nodetiming.py
@@ -6,14 +6,29 @@ import nano
 
 
 class BlockAlreadyInHistoryException(Exception):
+	"""
+	This is a specific error that had no standard error appropriate for it
+	"""
     def __init___(self,dErrorArguments):
         Exception.__init__(self,"Block hash is not frontier {0}".format(dErrArguments))
         self.dErrorArguments = dErrorArguements
 
 
-def transcation_general(node_IP, account_address, current_hash, start_timestamp)
+
+def transcation_general(node_IP, account_address, current_hash, start_timestamp):
+	"""
+	This to time the sending and recieving of blocks
+	@return delta of how long send or receive took in seconds
+	@param node_IP address of client
+	@param account_address account address
+	@param current_hash hash of the part of transcation we are interested in
+	@param start_timestamp the start of time the transaction
+	@param raise ValueError for unable to get the history for any reason
+	@raise BlockAlreadyInHistoryException for when we have missed the  transaction
+	"""
 	rpc_node = nano.rpc.Client(node_IP)
 
+		#Sleep times are for incase we are still waiting for the transcation to go through
 		backoff_sleep_values = [1,1,2,3,5,8]/2.0
 		for sleep_value in backoff_sleep_values:
 
@@ -43,10 +58,20 @@ def transcation_general(node_IP, account_address, current_hash, start_timestamp)
 
 
 def time_transaction_recieve(transcation):
+	"""
+	Will get the time delta of the receiving block
+	@param transaction django model of a transaction
+	@return delta in seconds of how long it took to get the receiving block
+	"""
 	return transcation_general(transaction.node_IP, transcation.destination.address, transaction.transaction_hash_receiving, transcation.start_timestamp)
 
 
 
 def time_transaction_send(transcation):
+	"""
+	Will get the time delta of the sendig block
+	@param transaction django model of a transacton
+	@return delta in seconds of how long it took to get the sending block
+	"""
 	return transcation_general(transaction.node_IP, transaction.origin.address, transaction.transaction_hash_sending,transaction.start_timestamp)
 
