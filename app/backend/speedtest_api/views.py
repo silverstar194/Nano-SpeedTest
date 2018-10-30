d5d736582ed2792550a6bd675728fad8d7ad655d
Matty Caulfield <caulfield2@wisc.edu>
2018-10-30 13:35:27 -0500

    Added serializer and view for initial API endpoint

--- app/backend/speedtest_api/views.py
+++ app/backend/speedtest_api/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+from speedtest_api.models import Transaction
+from speedtest_api.serializers import TransactionSerializer
+
+from datetime import datetime
+
+
+@api_view(['POST'])
+def send_transaction(request):
+    """
+    Send a transaction to the database
+    """
+
+    # Dummy JSON for the UI guys to test with
+    transaction = {
+        "origin": "123456789",
+        "destination": "987654321",
+        "start_timestamp": datetime.utcnow(),
+        "end_timestamp": datetime.utcnow(),
+        "amount": 13,
+        "initiated_by": "192.168.0.1"
+    }
+
+    serializer = TransactionSerializer(data=transaction)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
