14c3486ce49ee8f04ddff2200ab58a7965c27a89
Sean Felderman <sean.felderman@gmail.com>
2018-11-01 14:38:24 -0500

    Gets transaction/random to display on the map and update the table correctly

--- app/frontend/src/components/StatsPage.jsx
+++ app/frontend/src/components/StatsPage.jsx
@@ -1,41 +1,58 @@
 import React, {Component} from 'react';
-import Header from './Header'
+import Header from './Header';
 import PropTypes from 'prop-types';
 import TableRow from './TableRow';
 import '../styles/StatsPage.css';
 import Map from './Map';
 import {connect} from 'react-redux';
 import {addTransaction} from "../actions/table";
-import uuid from 'uuid'
-
+import uuid from 'uuid';
 
 class StatsPage extends Component {
     state = {
-        cities: [{
-            name: 'japan',
-            coords: {
-                lat: 36.2048,
-                lng: 138.2529
-            }
-        },
-            {
-                name: 'sf',
-                coords: {
-                    lat: 37.768249,
-                    lng: -122.445145
-                }
-            }
-        ]
+
     };
 
     render() {
+        let cities = {};
+        const {table} = this.props;
+        if (table.length) {
+            const transaction = table[table.length - 1]; // grab the last entry that we have loaded
+            const origin = transaction['origin'];
+            const originIndex = origin.indexOf(',');
+            const destination = transaction['destination'];
+            const destinationIndex = destination.indexOf(',');
+
+            cities = {
+                origin: {
+                    name: 'Mumbai', //hard coded for now
+                    coords: {
+                        lat: parseFloat(origin.substring(0, originIndex)),
+                        lng: parseFloat(origin.substring(originIndex + 1))
+                    }
+                },
+                destination: {
+                    name: 'Virginia', //hard coded for now
+                    coords: {
+                        lat: parseFloat(destination.substring(0, destinationIndex)),
+                        lng: parseFloat(destination.substring(destinationIndex + 1))
+                    }
+                }
+            };
+        }
         // render the jsx
         return (
             <div className='StatsPage'>
                 <Header/>
-                <h2 className='map-header page-header text-left'>Sent from Japan to San Francisco</h2>
+                {
+                    Object.keys(cities).length ?
+                        <h2 className='map-header page-header text-left'>
+                            Sent from {cities.origin.name} to {cities.destination.name}
+                        </h2>
+                        : null
+                }
                 <div className='nano-container'>
-                    <Map cities={this.state.cities}/>
+                    <Map cities={cities}/>
                 </div>
                 <div className='nano-container'>
                     <table className='table'>
@@ -52,7 +69,7 @@ class StatsPage extends Component {
                         </thead>
                         <tbody>
                         {
-                            this.props.table.map((props, index) => {
+                            table.map((props, index) => {
                                 props.index = index + 1;
                                 return <TableRow key={props.hash} {...props}/>;
                             })
