0c6164952bc715a7be8a9d000958fee01e31bf3d
Matty Caulfield <caulfield2@wisc.edu>
2018-11-02 21:59:09 -0500

    Added tests for account, nodes, and wallets

--- app/backend/speedtest_api/tests/test_accounts.py
+++ app/backend/speedtest_api/tests/test_accounts.py
@@ -1,15 +1,46 @@
 from django.test import TestCase
 
+from speedtest_api.models import Account
+from speedtest_api.models import Node
+from speedtest_api.models import Wallet
+
 
 class TestAccounts(TestCase):
     def setUp(self):
-        pass
-    
+        test_node = Node.objects.create(id=1,
+                                        IP="http://127.0.0.1:7076",
+                                        latitude= 1.1,
+                                        longitude=1.1,
+                                        location_name="Testville, USA")
+
+        test_wallet = Wallet.objects.create(node=test_node,
+                                            wallet_id=1)
+
+        test_account_1 = Account.objects.create(id=1,
+                                                wallet=test_wallet,
+                                                address=
+                                                "xrb_3er5ka9cx6nxtcfapcj77za3n4ne74xbp9b6sbxnuzan9iorxj3xoyy6h1n1",
+                                                current_balance=100,
+                                                POW="POW")
+
+        test_account_2 = Account.objects.create(id=2,
+                                                wallet=test_wallet,
+                                                address=
+                                                "xrb_3stk814q3ksqopqgeemzk7kduyjzhu355dmmon3ypco3dwbrm6c93wgspsy6",
+                                                current_balance=0,
+                                                POW="POW")
+
     def test_accounts_create(self):
-        pass
+        Account.objects.create(id=3,
+                               wallet=Wallet.objects.get(id=1),
+                               address="xrb_3stk814q3ksqopqgeemzk7kduyjzhu355dmmon3ypco3dwbrm6c93wgspsy6",
+                               current_balance=100,
+                               POW="POW")
     
     def test_accounts_get(self):
-        pass
+        account = Account.objects.get(id=1)
+        assert account.current_balance == 100
     
     def test_accounts_get_all(self):
-        pass
+        accounts = Account.objects.all()
+        assert accounts.count() == 2
--- app/backend/speedtest_api/tests/test_wallets.py
+++ app/backend/speedtest_api/tests/test_wallets.py
@@ -1,15 +1,31 @@
 from django.test import TestCase
 
+from speedtest_api.models import Node
+from speedtest_api.models import Wallet
+
+
 
 class TestWallet(TestCase):
     def setUp(self):
-        pass
+        test_node = Node.objects.create(id=1,
+                                        IP="http://127.0.0.1:7076",
+                                        latitude=1.1,
+                                        longitude=1.1,
+                                        location_name="Testville, USA")
+
+        test_wallet_1 = Wallet.objects.create(node=test_node,
+                                              wallet_id=1)
+
+        test_wallet_2 = Wallet.objects.create(node=test_node,
+                                              wallet_id=2)
     
     def test_wallets_create(self):
-        pass
+        Wallet.objects.create(node=Node.objects.get(id=1), wallet_id=3)
     
     def test_wallets_get(self):
-        pass
+        wallet = Wallet.objects.get(wallet_id=1)
+        assert wallet.node.id == 1
     
     def test_wallets_get_all(self):
-        pass
+        wallets = Wallet.objects.all()
+        assert wallets.count() == 2
