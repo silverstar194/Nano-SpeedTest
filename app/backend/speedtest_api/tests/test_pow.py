0fade59f686e6c7a9e8cc3deb437ae0e036e7343
Mitchell Mosure <hctimit@gmail.com>
2018-11-02 19:32:26 -0500

    test: added basic pow and transaction service tests

--- app/backend/speedtest_api/tests/test_pow.py
+++ app/backend/speedtest_api/tests/test_pow.py
@@ -1,12 +1,11 @@
 from django.test import TestCase
 
+from speedtest_api.services._pow import POWService
+
 
 class TestPOW(TestCase):
     def setUp(self):
         pass
     
     def test_dpow(self):
-        pass
-    
-    def test_pow(self):
-        pass
+        self.assertIsNotNone(POWService._get_dpow('F076D8F6254F089A8E66D0C934FA63D927F4458FC1D96815066D83B3658ABA26'))
--- app/backend/speedtest_api/tests/test_transactions.py
+++ app/backend/speedtest_api/tests/test_transactions.py
@@ -1,15 +1,45 @@
 from django.test import TestCase
 
+from speedtest_api.services import *
+import speedtest_api.models as models
+
 
 class TestTransactions(TestCase):
     def setUp(self):
-        pass
+        curr_Node = models.Node.objects.create(IP="http://127.0.0.1:7076", latitude= 1.1, longitude=1.1, location_name='')
+        curr_Node2 = models.Node.objects.create(IP="http://127.0.0.2:7076", latitude= 1.0, longitude=1.0, location_name='')
+
+        wallet_sending = models.Wallet.objects.create(node=curr_Node)
+        wallet_receiving = models.Wallet.objects.create(node=curr_Node2)
+
+        account_sending = models.Account.objects.create(wallet=wallet_sending, 
+            address="xrb_3er5ka9cx6nxtcfapcj77za3n4ne74xbp9b6sbxnuzan9iorxj3xoyy6h1n1",
+            current_balance=0, 
+            POW="POW")
+
+        account_receiving = models.Account.objects.create(wallet=wallet_receiving, 
+            address="xrb_3stk814q3ksqopqgeemzk7kduyjzhu355dmmon3ypco3dwbrm6c93wgspsy6", 
+            current_balance=0, 
+            POW="POW")
+
+        known_transaction = models.Transaction.objects.create(
+            origin=account_sending,
+            destination=account_receiving,
+            amount=1000,
+            initiated_by='127.0.0.1'
+        )
     
     def test_transactions_create(self):
-        pass
+        transaction_test = transactions.new_transaction_random('127.0.0.1')
+
+        self.assertIsNotNone(models.Transaction.objects.get(id=transaction_test.id))
     
     def test_transactions_get(self):
-        pass
+        transaction_test = transactions.new_transaction_random('127.0.0.1')
+
+        self.assertIsNotNone(transactions.get_transaction(id=transaction_test.id))
     
     def test_transactions_get_all(self):
-        pass
+        transaction_test = transactions.new_transaction_random('127.0.0.1')
+
+        self.assertIs(len(transactions.get_transactions()) > 1, True)
