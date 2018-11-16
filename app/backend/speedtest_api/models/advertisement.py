61864c71241108247c0a363659a182cfcd437155
Matty Caulfield <caulfield2@wisc.edu>
2018-11-15 20:24:41 -0600

    Made migrations for Advertisement

--- app/backend/speedtest_api/models/advertisement.py
+++ app/backend/speedtest_api/models/advertisement.py
@@ -5,3 +5,7 @@ class Advertisement(models.Model):
     message = models.TextField()
     URL = models.URLField
     title = models.TextField()
+    start_timestamp = models.BigIntegerField(null=True, default=None)
+    end_timestamp = models.BigIntegerField(null=True, default=None)
+    company = models.TextField()
+    tokens = models.IntegerField(null=True, default=None)
