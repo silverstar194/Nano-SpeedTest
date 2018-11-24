f0ee80b68907f63c80c53cf06c66a24b2f718063
Sean Felderman <sean.felderman@gmail.com>
2018-11-24 10:48:08 -0800

    Updated unit tests

--- app/frontend/src/reducers/pastResults.js
+++ app/frontend/src/reducers/pastResults.js
@@ -8,15 +8,12 @@ export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case ADD_ALL_PAST_RESULTS:
             const newState = {};
+            newState.totalTransactions = action.totalTransactions;
+            newState.globalAverage = action.globalAverage;
+
             // use the old state value if nothing has updated to prevent re-render
             newState.pastTransactions = (action.pastTransactions.length === state.pastTransactions.length)
                 ? state.pastTransactions : action.pastTransactions;
-
-            newState.totalTransactions = (action.totalTransactions === state.totalTransactions)
-                ? state.totalTransactions : action.totalTransactions;
-
-            newState.globalAverage = (action.globalAverage === state.globalAverage)
-                ? state.globalAverage : action.globalAverage;
 
             return {...newState};
         case APPEND_PAST_RESULTS:
