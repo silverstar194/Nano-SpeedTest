352b6d04035fca1d98784a75d36b7d2f95a8074c
silverstar194 <maloney4@wisc.edu>
2018-12-14 15:57:29 -0600

    custom markers

--- app/frontend/src/components/CurrentTransactions/Map.jsx
+++ app/frontend/src/components/CurrentTransactions/Map.jsx
@@ -20,19 +20,42 @@ const makeCoordinate = (point) => {
     return `${point.latitude},${point.longitude}`;
 };
 
+const countUses = (transaction, transactions, isOrigin) => {
+    let duplicatesCountOrigin = 0
+    let duplicatesCountDestination = 0
+    for(let i = 0; i < transactions.length; i++){
+       if(transaction.origin.nodeLocation === transactions[i].origin.nodeLocation || transaction.origin.nodeLocation === transactions[i].destination.nodeLocation){
+            duplicatesCountOrigin+=1;
+       }
+
+        if(transaction.destination.nodeLocation === transactions[i].destination.nodeLocation || transaction.destination.nodeLocation === transactions[i].origin.nodeLocation){
+            duplicatesCountDestination+=1;
+       }
+    }
+
+    if(isOrigin){
+        return duplicatesCountOrigin
+    }
+
+    return duplicatesCountDestination;
+}
+
 const Map = ({transactions}) => {
     let transactionPairs = [];
+    let markerPairs = []
+
     transactions.forEach((trans) => {
         transactionPairs.push(`${makeCoordinate(trans.origin)}|${makeCoordinate(trans.destination)}`);
+        markerPairs.push(`markers=color:0x4A90E2|label:${countUses(trans, transactions, true)}|${makeCoordinate(trans.origin)}&markers=color:0x4A90E2|label:${countUses(trans, transactions, false)}|${makeCoordinate(trans.destination)}`);
     });
+
     const locations = transactionPairs.join('|'); // gives all locations in one single string
-    const markers = `size:small|${locations}`;
     const paths = transactionPairs.join('&path='); // each needs to be draw separately
-
-
+    const markers = markerPairs.join("&")
+    
     return (
         <img className='GoogleMap'
-        src={`${base}?size=${size}&scale=${scale}&markers=${markers}&path=${paths}&key=${googleKey}`}
+        src={`${base}?size=${size}&scale=${scale}&${markers}&path=${paths}&key=${googleKey}`}
         alt={'Google Map Showing Destination and Origin'} />
     );
 };
