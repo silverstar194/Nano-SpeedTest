942dd8c11ec512337dc9b80c4835798ab96fb6de
silverstar194 <maloney4@wisc.edu>
2019-02-22 23:41:54 +1100

    added to node

--- app/backend/speedtest_api/models/node.py
+++ app/backend/speedtest_api/models/node.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Node(models.Model):
     URL = models.CharField(max_length=512)
+    IP = models.CharField(max_length=512)
     latitude = models.DecimalField(decimal_places=6, max_digits=10)
     longitude = models.DecimalField(decimal_places=6, max_digits=10)
     location_name = models.CharField(max_length=256, default=None)
