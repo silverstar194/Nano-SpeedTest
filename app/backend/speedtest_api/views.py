b86ef40fa398700e6fc1dfca96d4925527326df2
silverstar194 <maloney4@wisc.edu>
2019-02-22 23:20:14 +1100

    added

--- app/backend/speedtest_api/views.py
+++ app/backend/speedtest_api/views.py
@@ -360,6 +360,7 @@ def callback(request):
         cache_key = body["hash"]+"_"+client_ip  # needs to be unique
         cache_time = 600  # time in seconds for cache to be valid
         data = cache.get(cache_key)  # returns None if no key-value pair
+        logger.info(cache_key)
         if data:
             logger.error("Block already in cache. Duplicated broadcast. Key %s" % (cache_key))
             return JsonResponse({'message': "Block already cached"},
