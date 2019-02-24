d1828be69cad964a776b8e06ae10bb6e7863b673
silverstar194 <maloney4@wisc.edu>
2019-02-24 23:29:32 +1100

    spacing

--- app/backend/speedtest_api/services/_nodetiming.py
+++ app/backend/speedtest_api/services/_nodetiming.py
@@ -12,7 +12,7 @@ import nano
 logger = logging.getLogger(__name__)
 
 def transaction_general(node_URL, node_IP, account_address, current_hash, start_timestamp):
-	"""
+    """
 	This to time the sending and recieving of blocks
 	@return delta of how long send or receive took in seconds
 	@param node_URL address of client
@@ -23,48 +23,43 @@ def transaction_general(node_URL, node_IP, account_address, current_hash, start_
 	@raise Exception for when we have missed the transaction
 	"""
 
+    backoff_sleep_values =[2] + [.5]*35
+    for backoff_sleep_values in backoff_sleep_values:
+        cache_key = current_hash+"_"+node_IP  # needs to be unique
+        end_time = cache.get(cache_key)  # returns None if no key-value pair
+        logger.info("Checking for key %s" % (cache_key))
 
-	#Sleep times are incase we are still waiting for the transcation to go through
-    rpc_node = nano.rpc.Client(node_URL)
+        if end_time:
+            logger.info("Used cache %s %s" % (current_hash, account_address))
+            return end_time
 
-	backoff_sleep_values =[2] + [.5]*35
-	for sleep_value in backoff_sleep_values:
+        address = account_address
+        hash_of_block = current_hash
 
-		cache_key = current_hash+"_"+node_IP  # needs to be unique
-		end_time = cache.get(cache_key)  # returns None if no key-value pair
-		logger.info("Checking for key %s" % (cache_key))
-
-		if end_time:
-			logger.info("Used cache %s %s" % (current_hash, account_address))
-			return end_time
-
-        ##Timing with RCP call
-		address = account_address
-		hash_of_block = current_hash
-
-		try:
-			history_curr_account = rpc_node.account_history(address,
+        try:
+            history_curr_account = rpc_node.account_history(address,
 															count=5)  # magic assuming that if it is not 5 back it hasn't been received
-		except:
-			logger.error('Unable to get history hash: %s, account: %s' % (current_hash, account_address))
-			raise ValueError("Unable to get history hash: %s, account: %s" % (current_hash, account_address))
+        except:
+            logger.error('Unable to get history hash: %s, account: %s' % (current_hash, account_address))
+            raise ValueError("Unable to get history hash: %s, account: %s" % (current_hash, account_address))
 
-		frontier_hash = history_curr_account[0][u'hash']
+        frontier_hash = history_curr_account[0][u'hash']
 
-		if hash_of_block == frontier_hash:
+        if hash_of_block == frontier_hash:
             logger.info("Used RPC %s %s" % (current_hash, account_address))
-			end_time = int(rpc_node.account_info(address)[u'modified_timestamp']) * 1000
-			return end_time
+            end_time = int(rpc_node.account_info(address)[u'modified_timestamp']) * 1000
 
-		for value in history_curr_account:
-			if value[u'hash'] is hash_of_block:
-				logger.error("Unable to get hash %s" % hash_of_block)
-				raise Exception("Unable to get hash %s" % hash_of_block)
+        return end_time
 
-		time.sleep(sleep_value)
+        for value in history_curr_account:
+            if value[u'hash'] is hash_of_block:
+                logger.error("Unable to get hash %s" % hash_of_block)
+                raise Exception("Unable to get hash %s" % hash_of_block)
 
-	logger.error("Transaction was never found %s " % hash_of_block)
-	raise Exception("Transaction never found %s " % hash_of_block) 
+        time.sleep(sleep_value)
+
+    logger.error("Transaction was never found %s " % hash_of_block)
+    raise Exception("Transaction never found %s " % hash_of_block)
 
 def time_transaction_receive(transaction):
 	"""
@@ -75,8 +70,8 @@ def time_transaction_receive(transaction):
 	"""
 	end_time = transaction_general(transaction.origin.wallet.node.URL,
 		transaction.origin.wallet.node.IP_ADD,
-		transaction.destination.address, 
-		transaction.transaction_hash_receiving, 
+		transaction.destination.address,
+		transaction.transaction_hash_receiving,
 		transaction.start_receive_timestamp)
 
 	if transaction.start_receive_timestamp > end_time:
@@ -97,7 +92,7 @@ def time_transaction_send(transaction):
 	"""
 	end_time = transaction_general(transaction.destination.wallet.node.URL,
 	transaction.destination.wallet.node.IP_ADD,
-	 transaction.origin.address, 
+	 transaction.origin.address,
 	 transaction.transaction_hash_sending,
 	 transaction.start_send_timestamp)
 
