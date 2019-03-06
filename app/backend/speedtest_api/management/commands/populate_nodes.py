84c1903cb3c7114e9c8575bdbebb8420fb7dcd8f
silverstar194 <maloney4@wisc.edu>
2019-03-06 15:38:04 +1100

    updates account population

--- app/backend/speedtest_api/management/commands/populate_nodes.py
+++ app/backend/speedtest_api/management/commands/populate_nodes.py
@@ -4,12 +4,13 @@ from ...models.account import Account
 from ...services.nodes import get_nodes
 from ...services.wallets import new_wallet, get_wallets
 from ...services._pow import POWService
-from ...services.accounts import sync_accounts, get_accounts, new_account
+from ...services.accounts import sync_accounts, get_accounts, new_account, get_account
 from ...services.transactions import simple_send
+from django.conf import settings as settings
 
 import nano
-import sys
 import time
+import requests
 import random
 
 class PopulationException(Exception):
@@ -17,6 +18,7 @@ class PopulationException(Exception):
         Exception.__init__(self, "Error occurred in the node population process.")
 
 class Command(BaseCommand):
+
     help = "Generate a new wallet for every node, add accounts and distribute funds"
 
     def handle(self, *args, **options):
@@ -25,8 +27,9 @@ class Command(BaseCommand):
         These new accounts are unopened and require balance_accounts to be run in order to give them nano.
         POW is also generated on these accounts.
         Funds are then send to open the accounts.
+        NOTE: This script is buggy and needs monitoring.
         """
-        number_accounts_per_node = 200
+        number_accounts_per_node = 150
 
         nodes_list = get_nodes()
         wallets_list = get_wallets()
@@ -41,7 +44,7 @@ class Command(BaseCommand):
                 wallet = new_wallet(node=node)
 
             for i in range(number_accounts_per_node):
-                new_account(wallet=wallet)
+                print("Created %s" % (new_account(wallet=wallet)))
 
         all_accounts = list(get_accounts())
         funding_account = all_accounts[0]
@@ -55,55 +58,66 @@ class Command(BaseCommand):
         while funding_account.current_balance == 0:
             sync_accounts()
             funding_account = Account.objects.filter(address=funding_account.address)[0]
-            time.sleep(120)
+            time.sleep(5)
 
 
-        #PoW
         rpc = nano.rpc.Client(funding_account.wallet.node.URL)
         for i in range(6):
             try:
                 frontier = rpc.frontiers(account=funding_account.address, count=1)[funding_account.address]
                 if funding_account.POW is None or not rpc.work_validate(work=funding_account.POW, hash=frontier):
-                    POWService.enqueue_account(address=funding_account.address, frontier=frontier)
+                    print("Generating PoW account %s " % (funding_account.address))
+
+                    data = {
+                     'hash': str(frontier),
+                     'key': settings.DPOW_API_KEY
+                    }
+
+                    res = requests.post(url=settings.DPOW_ENDPOINT, json=data, timeout=15)
+                    funding_account.POW = res.json()['work']
+                    funding_account.save()
                     break
             except Exception:
                 if i == 5:
                     print('dPoW failure account %s unlocked without PoW' % funding_account.address)
                     funding_account.unlock()
-                    sys.exit()
 
-        while not funding_account.POW:
-            funding_account = Account.objects.get(address=funding_account.address)
-            time.sleep(7)
+            while not funding_account.POW:
+                funding_account = Account.objects.get(address=funding_account.address)
+                time.sleep(1)
 
         empty_accounts = Account.objects.filter(current_balance=0).all()
-        # Distribute funds between accounts to open them
+
+        #Distribute funds between accounts to open them
         amount = funding_account.current_balance / len(empty_accounts[:])
 
         random.shuffle(all_accounts) # spread opening load across nodes
+        print("Accounts empty %s " % (len(empty_accounts[:])))
         for account_init in all_accounts:
             # Already opened
             if account_init.current_balance > 0:
+                print("Skipping")
                 continue
-            simple_send(funding_account, account_init.address, int(amount)) ##Using send simple allows node to generate open block for us
+            try:
+                frontier = rpc.frontiers(account=funding_account.address, count=1)[funding_account.address]
+                if funding_account.POW is None or not rpc.work_validate(work=funding_account.POW, hash=frontier):
 
-        ## Wait for everything to clear the network
-        all_cleared = False
-        count = 0
-        while not all_cleared and count < 20:
-            count+=1
-            # Resync to acquire balances
-            all_cleared = True
-            sync_accounts()
-            for account in get_accounts():
-                if not account.current_balance == amount:
-                    all_cleared = False
-                    break
-            time.sleep(10)
+                    data = {
+                     'hash': str(frontier),
+                     'key': settings.DPOW_API_KEY
+                    }
 
+                    res = requests.post(url=settings.DPOW_ENDPOINT, json=data, timeout=15)
+                    funding_account.POW = res.json()['work']
+                    funding_account.save()
+            except Exception:
+                if i == 5:
+                    print('dPoW failure account %s unlocked without PoW' % funding_account.address)
+                    funding_account.unlock()
+            count = 0
+            while not funding_account.POW and count < 5:
+                funding_account = Account.objects.get(address=funding_account.address)
+                count += 1
+                time.sleep(1)
 
-        if count == 20:
-            print("Some transactions failed...")
-            sys.exit()
-
-        POWService.POW_accounts()
+            simple_send(funding_account, account_init.address, int(amount), generate_PoW=False) ##Using send simple allows node to generate open block for us
