340d32404ece4397859f3c3bcc05cb8706b978f4
silverstar194 <maloney4@wisc.edu>
2019-12-24 21:09:31 -0800

    added limit

--- droplet_resize.py
+++ droplet_resize.py
@@ -57,15 +57,17 @@ for droplet in node_droplets:
 
 	if percent_sync < UNSYNC_LIMIT or unchecked > UNCHECKED_LIMIT and droplet.size['slug'] != instance_large:
 		logging.info("Resizing {0} to {1}".format(location, instance_large))
-		droplet.resize(instance_large, disk=True, return_dict=False)
+		droplet.resize(instance_large, disk=False, return_dict=False)
 		wait_on_resize(droplet)
+		time.sleep(120)
 		droplet.power_on()
 		logging.info("Turning on {0}".format(location))
 
 	if percent_sync > UNSYNC_LIMIT and unchecked < UNCHECKED_LIMIT and droplet.size['slug'] != instance_small:
 		logging.info("Resizing {0} to {1}".format(location, instance_small))
-		droplet.resize(instance_small, disk=True, return_dict=False)
+		droplet.resize(instance_small, disk=False, return_dict=False)
 		wait_on_resize(droplet)
+		time.sleep(120)
 		droplet.power_on()
 		logging.info("Turning on {0}".format(location))
 
